e4ffd858d4f0ab38a6507c32545127c731dbf2bd
楊奕儒 <98101443+ben25000233@users.noreply.github.com>
2024-09-07 12:57:56 +0800

    Add files via upload

---
+++ multimodal/mini_main.py
@@ -0,0 +1,48 @@
+from __future__ import print_function
+import argparse
+import yaml
+import os
+
+from trainers.selfsupervised import selfsupervised
+from trainers.predict_scoop import predict_scoop
+from trainers.predict_spillage import predict_spillage
+
+
+if __name__ == "__main__":
+
+    # Load the config file
+    parser = argparse.ArgumentParser(description="Sensor fusion model")
+    parser.add_argument("--config", help="YAML config file")
+    parser.add_argument("--notes", default="", help="run notes")
+    parser.add_argument("--dev", type=bool, default=False, help="run in dev mode")
+    parser.add_argument(
+        "--continuation",
+        type=bool,
+        default=False,
+        help="continue a previous run. Will continue the log file",
+    )
+    parser.add_argument("--type", default="spillage")
+    args = parser.parse_args()
+
+    
+    # Add the yaml to the config args parse
+    with open(args.config) as f:
+        configs = yaml.safe_load(f)
+
+
+    # Merge configs and args
+    for arg in vars(args):
+        configs[arg] = getattr(args, arg)
+
+    # Initialize the trainer
+    if args.type == "spillage":
+        trainer = predict_spillage(configs)
+    else : 
+        trainer = predict_scoop(configs)
+        
+    if configs["load"] :
+        print("start validatioln")
+        trainer.validate()
+    else : 
+        print("start training")
+        trainer.train()
